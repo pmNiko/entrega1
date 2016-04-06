6625f6ab0bcfd770c573f41ae8ebebb5e8353485
Nikol@s <nikolas090189@gmail.com>
2016-04-05 22:38:56 -0300

    Initial commit

---
+++ cmd.rb
@@ -0,0 +1,17 @@
+require 'highline/import'
+
+salir = false
+
+while !salir do
+
+choose do |menu|
+	menu.prompt = "Desea salir?"
+	menu.choice(:si) {
+		salir = true
+		say("Gracias por venir")
+	}
+	menu.choice(:No) do
+		say("Ok, nos quedamos")
+	end
+end
+end
